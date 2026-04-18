297943484c8b64b30cd96558906649782a50e8ff
Velora AI <jrdnmcphrsn@yahoo.com>
2026-04-17 21:59:49 -0600

    replace button with link to Calendly

---
+++ app/book/page.tsx
@@ -0,0 +1,20 @@
+export default function Book() {
+  return (
+    <main
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Helvetica, sans-serif"
+      }}
+    >
+      <iframe
+        src="https://calendly.com"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+      />
+    </main>
+  );
+}
